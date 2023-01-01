0bd493d6bf46b044661eb0787d0aae0e90ad5e0e
rupam-seal <rupam.x.seal@gmail.com>
2023-01-01 09:19:10 +0530

    refactoring code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 const App = () => {
-  const a = 'click button to view advice';
+  const a = 'click below to get an advice';
   const [advice, setAdvice] = useState(a);
 
   const fetchAdvice = () => {
@@ -20,8 +20,8 @@ const App = () => {
   return (
     <div>
       <div className="container">
-        <h1>{advice}</h1>
-        <button onClick={fetchAdvice}>Change</button>
+        <h2>{advice}</h2>
+        <button onClick={fetchAdvice}>GIVE ME ADVICE</button>
       </div>
     </div>
   );
